f890883285954dfa22c988aa6d4637ed47e69ca4
Mateus Henrique Chaves Araujo Morais <mateushenriquechaves1@gmail.com>
2021-02-23 19:53:03 -0300

    adicionando o arraylist de movimentos

--- src/pojos/Almoxarifado.java
+++ src/pojos/Almoxarifado.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 public class Almoxarifado {
 
     private ArrayList<Produto> produtos = new ArrayList<>();
+    private ArrayList<Movimento> movimentos = new ArrayList<>();
 
     public Almoxarifado(ArrayList<Produto> produtos, ArrayList<Movimento> movimentos) {
         this.movimentos = movimentos;
