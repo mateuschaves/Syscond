8feed79039a39b89656451b13591ea20f5b28446
Eduardo Marinho <eduardommmarinho@gmail.com>
2021-02-24 22:12:04 -0300

    troca de funções de Movimento para Dados

--- src/classeauxiliar/Dados.java
+++ src/classeauxiliar/Dados.java
@@ -5,10 +5,56 @@
  */
 package classeauxiliar;
 
+import pojos.Produto;
+import java.util.Date;
+
 /**
  *
  * @author Mattskywalker
  */
 public class Dados {
+    private Produto produto;
+    private int tipo; // 1 entrada - 0 saida
+    private int quantidade;
+    private Date data;
     
+    
+    public Dados(Produto produto, int tipo, int quantidade, Date data) {
+        this.produto = produto;
+        this.tipo = tipo;
+        this.quantidade = quantidade;
+        this.data = data;
+    }
+    
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
 }
