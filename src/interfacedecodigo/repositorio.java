5911c7ba56afa6e9361a2960684c7b5cc9c47df6
Mattskywalker <Mattskywalker@Mattskywaker>
2021-02-27 19:12:22 -0300

    interface adicionada

--- src/interfacedecodigo/repositorio.java
+++ src/interfacedecodigo/repositorio.java
@@ -3,19 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package syscond;
+package interfacedecodigo;
 
 /**
  *
  * @author Mattskywalker
  */
-public class Syscond {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public interface repositorio {
+    
+    public void adicionar(Object objeto);
     
 }
