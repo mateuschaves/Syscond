9dd9c09a0813a3977208d6f50874386773f7b55d
Mattskywalker <Mattskywalker@Mattskywaker>
2021-03-03 16:29:57 -0300

    excepetions separadas por pacote

--- src/exceptions/apartamento/ApartamentoJaExistente.java
+++ src/exceptions/apartamento/ApartamentoJaExistente.java
@@ -1,4 +1,4 @@
-package exceptions;
+package exceptions.apartamento;
 
 public class ApartamentoJaExistente extends Exception{
     /**
--- src/exceptions/apartamento/ApartamentoNaoEncontrado.java
+++ src/exceptions/apartamento/ApartamentoNaoEncontrado.java
@@ -1,4 +1,4 @@
-package exceptions;
+package exceptions.apartamento;
 
 public class ApartamentoNaoEncontrado extends Exception{
     /**
