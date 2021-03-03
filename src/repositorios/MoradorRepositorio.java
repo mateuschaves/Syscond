7ecbade22b3f1b635b91dc8d492e48c23838e586
Mattskywalker <Mattskywalker@Mattskywaker>
2021-03-03 17:09:10 -0300

    pacote de repositorios criado

--- src/repositorios/MoradorRepositorio.java
+++ src/repositorios/MoradorRepositorio.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package negocios;
+package repositorios;
 
 import classeauxiliar.TipoMorador;
 import java.util.ArrayList;
