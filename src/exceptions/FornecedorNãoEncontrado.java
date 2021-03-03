921d4039f7251f0bd929f0dd9fbae17405784b23
Mattskywalker <Mattskywalker@Mattskywaker>
2021-03-03 16:03:50 -0300

    Exception criada

---
+++ src/exceptions/FornecedorNãoEncontrado.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+public class FornecedorNãoEncontrado extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public FornecedorNãoEncontrado(String mensagem) {
+		super(mensagem);
+	}
+	
+}
