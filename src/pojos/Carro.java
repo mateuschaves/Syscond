332849714c8bbd41ae12d379b8fbf5037b0949a4
Breno Araujo Campos <breno.araujocampos@upe.br>
2021-03-04 20:33:57 -0300

    Adicionados gets e sets mais o construtor de carro, testem e confiram para colocar na branch master! Vlw!

--- src/pojos/Carro.java
+++ src/pojos/Carro.java
@@ -14,6 +14,64 @@ public class Carro {
     private String modelo;
     private String placa;
     private String cor;
-    private Morador propietario;
+    private Morador proprietario;
     
-}
+    
+    
+    Carro(Morador proprietario, String modelo, String placa, String cor){
+    	
+    	this.proprietario = proprietario;
+    	this.modelo = modelo;
+    	this.placa = placa;
+    	this.cor = cor;
+    }
+
+	public String getModelo() {
+      
+		return this.modelo;
+		
+	}
+	
+	public void setModelo(String modelo){
+		
+		this.modelo = modelo;
+		
+	}
+	
+	public String getplaca() {
+	      
+		return this.placa;
+		
+	}
+	
+	public void setplaca(String placa){
+		
+		this.placa = placa;
+		
+	}
+	
+	
+	public String getCor() {
+	      
+		return this.cor;
+		
+	}
+	
+	public void setCor(String cor){
+		
+		this.cor = cor;
+		
+	}
+	
+	
+	public Morador getProprietario() {
+	      
+		return this.proprietario;
+		
+	}
+	
+	public void setProprietario(Morador proprietario){
+		
+		this.proprietario = proprietario;	
+	}	
+}	
