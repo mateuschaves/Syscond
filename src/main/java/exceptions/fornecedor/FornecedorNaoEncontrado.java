2f78ed407d4013bcc6d26659a318416c2e2004f2
Mattskywalker <mateusejesus@hotmail.com.br>
2021-03-18 14:56:51 -0300

    bug do padrão de codificação corrigido;

--- src/main/java/exceptions/fornecedor/FornecedorNaoEncontrado.java
+++ src/main/java/exceptions/fornecedor/FornecedorNaoEncontrado.java
@@ -1,15 +1,14 @@
-/*spackage exceptions.fornecedor;
+package exceptions.fornecedor;
 
 public class FornecedorNaoEncontrado extends Exception{
 
 	/**
 	 * 
 	 */
-	/*private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 	
 	public FornecedorNaoEncontrado(String mensagem) {
 		super(mensagem);
 	}
 	
 }
-*/
