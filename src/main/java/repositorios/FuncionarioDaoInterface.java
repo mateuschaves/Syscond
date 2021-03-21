1f77c8534170489282d91a65f8e29d6e814cfe24
Eduardo Marinho <eduardommmarinho@gmail.com>
2021-03-21 18:32:27 -0300

    Redefinindo a interface do repositório de funcionários

---
+++ src/main/java/repositorios/FuncionarioDaoInterface.java
@@ -0,0 +1,23 @@
+package repositorios;
+
+import java.util.ArrayList;
+
+import exceptions.funcionario.FuncionarioJaExistente;
+import exceptions.funcionario.FuncionarioNaoEncontrado;
+
+import pojos.Funcionario;
+
+public interface FuncionarioDaoInterface {
+
+    public void adicionar(Funcionario funcionario) throws FuncionarioJaExistente;
+
+    public void alterar(Funcionario funcionario) throws FuncionarioNaoEncontrado, FuncionarioJaExistente;
+
+    public ArrayList<Funcionario> listar() throws FuncionarioNaoEncontrado;
+
+    public Funcionario procurar(String id) throws FuncionarioNaoEncontrado;
+
+    public void remover(Funcionario funcionario) throws FuncionarioNaoEncontrado;
+
+    public void remover(String cpf) throws FuncionarioNaoEncontrado;
+}
