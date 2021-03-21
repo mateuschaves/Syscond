82bcc0cc3a2d2b43d3dd540b991ed579337faaf3
Eduardo Marinho <eduardommmarinho@gmail.com>
2021-03-21 18:33:44 -0300

    Criação de buscas nomeadas para Funcionario

--- src/main/java/pojos/Funcionario.java
+++ src/main/java/pojos/Funcionario.java
@@ -1,59 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pojos;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
-/**
- *
- * @author Mattskywalker
- */
+@NamedQueries({ @NamedQuery(name = "Funcionario.buscaPorCPF", query = "select f from Funcionario f where f.cpf = :cpf"),
+                @NamedQuery(name = "Funcionario.buscaTodos", query = "select f from Funcionario f") })
 @Entity
 public class Funcionario {
 
-    @Id
-    private String cpf;
-    @Column(nullable = false)
-    private String nome;
-    @Column(nullable = false)
-    private String funcao;
+        @Id
+        private String cpf;
+        @Column(nullable = false)
+        private String nome;
+        @Column(nullable = false)
+        private String funcao;
 
-    public Funcionario() {
-    }
+        public Funcionario() {
+        }
 
-    public Funcionario (String nome, String cpf, String funcao){
-           this.nome = nome;
-           this.cpf = cpf;
-           this.funcao = funcao;
-    }
-    
-    public String getNome(){
-           return this.nome;
-    }
-    
-    public void setNome(String nome){
-            this.nome = nome;    
-    }
-    
-    public String getCpf(){
-            return this.cpf;
-    } 
-    
-    public void setCpf(String cpf){
-            this.cpf = cpf;
-    }
-    
-    public String getFuncao(){
-            return this.funcao;
-    }
-    
-    public void setFuncao(String funcao){
-            this.funcao = funcao;
-    }
-    
+        public Funcionario(String nome, String cpf, String funcao) {
+                this.nome = nome;
+                this.cpf = cpf;
+                this.funcao = funcao;
+        }
+
+        public String getNome() {
+                return this.nome;
+        }
+
+        public void setNome(String nome) {
+                this.nome = nome;
+        }
+
+        public String getCpf() {
+                return this.cpf;
+        }
+
+        public void setCpf(String cpf) {
+                this.cpf = cpf;
+        }
+
+        public String getFuncao() {
+                return this.funcao;
+        }
+
+        public void setFuncao(String funcao) {
+                this.funcao = funcao;
+        }
+
 }
