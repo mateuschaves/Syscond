0a09d2b34b125f7487ed1a7235d18c24501d693c
Mattskywalker <mateusejesus@hotmail.com.br>
2021-03-28 16:11:15 -0300

    criada e suporte para novos metodos

---
+++ src/main/java/dao/ApartamentoDAO.java
@@ -0,0 +1,120 @@
+package dao;
+
+import exceptions.apartamento.ApartamentoJaExistente;
+import exceptions.apartamento.ApartamentoNaoEncontrado;
+import pojos.Apartamento;
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ApartamentoDAO implements ApartamentoDaoInterface{
+
+
+    @Override
+    public void adicionar(Apartamento apartamento) throws ApartamentoJaExistente {
+
+        EntityManager em = JPAUtil.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try{
+            tx.begin();
+            em.persist(apartamento);
+            tx.commit();
+        }catch (Exception a){
+            //a.printStackTrace();
+            System.err.println("Id já existente: " + a.getMessage());
+        }finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public void alterar(Apartamento apartamento) throws ApartamentoNaoEncontrado, ApartamentoJaExistente {
+        EntityManager em = JPAUtil.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try{
+
+            tx.begin();
+            em.merge(apartamento);
+            tx.commit();
+        }
+        catch(Exception e){
+            System.err.println("ERRO: " + e.getMessage());
+        }finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public List<Apartamento> listar() throws ApartamentoNaoEncontrado {
+
+        EntityManager em = JPAUtil.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        List<Apartamento> lista = new ArrayList<>();
+
+        try{
+
+            Query query = em.createQuery("select a from Apartamento a",Apartamento.class);
+            lista = query.getResultList();
+
+        }catch (Exception e){
+            e.getMessage();
+        }finally {
+            em.close();
+        }
+        return lista;
+    }
+
+    @Override
+    public Apartamento procurar(String numero) throws ApartamentoNaoEncontrado {
+
+        EntityManager em = JPAUtil.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        Apartamento retorno = new Apartamento();
+
+        try {
+            tx.begin();
+            retorno = em.find(Apartamento.class,numero);
+            tx.commit();
+
+        }catch(NullPointerException e){
+
+            System.err.println("ERRO: " + e.getMessage());
+
+        }finally {
+            em.close();
+        }
+        return retorno;
+    }
+
+    @Override
+    public void remover(Apartamento apartamento) throws ApartamentoNaoEncontrado {
+
+        EntityManager em = JPAUtil.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try{
+
+            tx.begin();
+            apartamento = em.merge(apartamento);
+            em.remove(apartamento);
+            tx.commit();
+
+        }catch (Exception e){
+            e.getMessage();
+        }finally {
+
+            em.close();
+
+        }
+
+    }
+}
