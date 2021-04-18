0def7acfc7811baa1f8b67d00eac15e55b68f7e2
Mateus Henrique Chaves Araujo Morais <mateushenriquechaves1@gmail.com>
2021-04-17 22:16:02 -0300

    Shoud create morador test case

--- src/test/java/MoradorTeste.java
+++ src/test/java/MoradorTeste.java
@@ -1,11 +1,22 @@
 import org.junit.Assert;
 import org.junit.Test;
 import pojos.Apartamento;
+import pojos.Carro;
 import pojos.Morador;
+import dao.MoradorDAO;
+import dao.ApartamentoDAO;
 
 import java.util.ArrayList;
+import java.util.List;
+
 
 public class MoradorTeste {
+
+    private Apartamento apartamentoMock = new Apartamento("1", "Primeiro", "B");
+    private List<Carro> carrosMock = new ArrayList<>();
+
+    private Morador moradorMock = new Morador("115", "Mateus Henrique", this.apartamentoMock, this.carrosMock);
+
     @Test
     public void testNome(){
         Assert.assertEquals(
@@ -18,6 +29,25 @@ public class MoradorTeste {
         Assert.assertEquals(
                 "1",
                 (new Morador("1", "Eduardo", new Apartamento(), new ArrayList<>())).getCpf());
+    }
+
+    @Test
+    public void shouldCreateMoradorSuccessfully(){
+        try {
+            ApartamentoDAO apartamentoDAO = new ApartamentoDAO();
+            apartamentoDAO.adicionar(this.apartamentoMock);
+
+            MoradorDAO moradorDao = new MoradorDAO();
+            moradorDao.adicionar(this.moradorMock);
+
+            List<Morador> moradores = moradorDao.listar();
+            Assert.assertEquals(1, moradores.size());
+            Morador morador = moradores.get(0);
+            Assert.assertEquals(this.moradorMock.getNome(), morador.getNome());
+            Assert.assertEquals(this.moradorMock.getCpf(), morador.getCpf());
+        }catch (Exception e) {
+
+        }
     }
 }
 
