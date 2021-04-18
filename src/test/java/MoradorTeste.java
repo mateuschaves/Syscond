d45f7144ec770f305cc61d731f98271827928ba3
Mateus Henrique Chaves Araujo Morais <mateushenriquechaves1@gmail.com>
2021-04-17 22:38:39 -0300

    More test cases to MoradorTeste.java

--- src/test/java/MoradorTeste.java
+++ src/test/java/MoradorTeste.java
@@ -12,10 +12,23 @@ import java.util.List;
 
 public class MoradorTeste {
 
-    private Apartamento apartamentoMock = new Apartamento("1", "Primeiro", "B");
-    private List<Carro> carrosMock = new ArrayList<>();
+    private final Apartamento apartamentoMock = new Apartamento("1", "Primeiro", "B");
+    private final List<Carro> carrosMock = new ArrayList<>();
+    private final Morador moradorMock = new Morador("115", "Mateus Henrique", this.apartamentoMock, this.carrosMock);
 
-    private Morador moradorMock = new Morador("115", "Mateus Henrique", this.apartamentoMock, this.carrosMock);
+    private final MoradorDAO moradorDao = new MoradorDAO();
+    private final ApartamentoDAO apartamentoDAO = new ApartamentoDAO();
+
+
+
+    private void createMorador() {
+        try {
+            this.apartamentoDAO.adicionar(this.apartamentoMock);
+            this.moradorDao.adicionar(this.moradorMock);
+        }catch (Exception e) {
+            System.out.println("createMorador failed");
+        }
+    }
 
     @Test
     public void testNome(){
@@ -34,20 +47,48 @@ public class MoradorTeste {
     @Test
     public void shouldCreateMoradorSuccessfully(){
         try {
-            ApartamentoDAO apartamentoDAO = new ApartamentoDAO();
-            apartamentoDAO.adicionar(this.apartamentoMock);
-
-            MoradorDAO moradorDao = new MoradorDAO();
-            moradorDao.adicionar(this.moradorMock);
-
-            List<Morador> moradores = moradorDao.listar();
+            this.createMorador();
+            List<Morador> moradores = this.moradorDao.listar();
             Assert.assertEquals(1, moradores.size());
             Morador morador = moradores.get(0);
             Assert.assertEquals(this.moradorMock.getNome(), morador.getNome());
             Assert.assertEquals(this.moradorMock.getCpf(), morador.getCpf());
         }catch (Exception e) {
-
+            System.out.println("shouldCreateMoradorSuccessfully failed");
         }
+    }
+
+    @Test
+    public void shouldListZeroMoradorWhenNoOneWasCreated(){
+        List<Morador> moradores = this.moradorDao.listar();
+        Assert.assertEquals(0, moradores.size());
+    }
+
+    @Test
+    public void shouldListOneMoradorWhenOneWasCreated(){
+        this.createMorador();
+        List<Morador> moradores = this.moradorDao.listar();
+        Assert.assertEquals(1, moradores.size());
+    }
+
+    @Test
+    public void shouldBeAbleToRemoveMorador(){
+        this.createMorador();
+        List<Morador> moradoresBeforeRemove = this.moradorDao.listar();
+        Assert.assertEquals(1, moradoresBeforeRemove.size());
+        this.moradorDao.remover(this.moradorMock);
+        List<Morador> moradoresAfterRemove = this.moradorDao.listar();
+        Assert.assertEquals(0, moradoresAfterRemove.size());
+    }
+
+    @Test
+    public void shouldBeAbleToUpdateMorador(){
+        String newName = "Mateus Henrique Editado";
+        this.createMorador();
+        this.moradorMock.setNome(newName);
+        this.moradorDao.alterar(this.moradorMock);
+        Morador moradorUpdated = this.moradorDao.procurar(this.moradorMock.getCpf());
+        Assert.assertEquals(newName, moradorUpdated.getNome());
     }
 }
 
