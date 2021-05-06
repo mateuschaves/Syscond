341f74cd9015be9969df937033ea15e90de027e2
Breno Araujo Campos <breno.araujocampos@upe.br>
2021-05-06 12:50:31 -0300

    Adição do comando "esq" na tela de consulta

--- src/main/java/view/MenuConsultaController.java
+++ src/main/java/view/MenuConsultaController.java
@@ -1,8 +1,22 @@
 package view;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 
-public class MenuConsultaController extends MenuController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MenuConsultaController extends MenuController implements Initializable {
+
+    @FXML
+    private BorderPane pane;
+    @FXML
+    private Label labelUsuario;
 
     public void backMenu() {
         setRoot("menu");
@@ -19,6 +33,15 @@ public class MenuConsultaController extends MenuController {
     public void exibirListarFornecedor(){}
 
     public void exibirListarAp(){}
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        writeUser(labelUsuario);
+        pane.setOnKeyPressed( (keyEvent) -> {
+            if(keyEvent.getCode() == KeyCode.ESCAPE)
+                backMenu();
+        } );
+    }
 
 
 }
