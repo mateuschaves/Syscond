bd56265902ecf06b0653c581d2304f6a37a96b42
Eduardo Marinho <eduardommmarinho@gmail.com>
2021-05-06 17:58:51 -0300

    popup de cadastrar mais uma vez FUNCIONÁRIOS

--- src/main/java/view/CadastroFuncionarioController.java
+++ src/main/java/view/CadastroFuncionarioController.java
@@ -1,11 +1,17 @@
 package view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 import negocios.FuncionarioNegocios;
 import pojos.Funcionario;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class CadastroFuncionarioController {
 
@@ -17,7 +23,7 @@ public class CadastroFuncionarioController {
     private TextField textFieldFuncao;
 
 
-    public void cadastrarFuncionario(){
+    public void cadastrarFuncionario() throws IOException {
 
         FuncionarioNegocios funcionarioNegocios = new FuncionarioNegocios();
 
@@ -28,6 +34,24 @@ public class CadastroFuncionarioController {
         Funcionario funcionario = new Funcionario(nome,cpf,funcao);
 
         funcionarioNegocios.cadastrar(funcionario);
+
+        Alert dialog = new Alert(Alert.AlertType.CONFIRMATION);
+        ((Stage)dialog.getDialogPane().getScene().getWindow()).getIcons().add(new Image("/img/syscondLogo.png"));
+        dialog.setHeaderText("Funcionário adicionado com sucesso.");
+        dialog.setContentText("Deseja adicionar outro funcionario?");
+        ButtonType oneMore = new ButtonType("Quero adicionar +1 funcionário");
+        ButtonType enoughCars = new ButtonType("Não, obrigado", ButtonBar.ButtonData.CANCEL_CLOSE);
+        dialog.getButtonTypes().setAll(oneMore, enoughCars);
+        Optional<ButtonType> result = dialog.showAndWait();
+        if(result.isPresent() && result.get() == oneMore){
+            textFieldCpf.setText("");
+            textFieldNome.setText("");
+            textFieldFuncao.setText("");
+            textFieldCpf.requestFocus();
+        }else{
+            dialog.close();
+            App.setRoot("menuCadastro");
+        }
     }
 
     public void voltar(){
